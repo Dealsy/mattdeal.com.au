87c94dad0ed293629d1272a918647df8ae6f1bec
dealsy <deal.sy@hotmail.com>
2018-04-16 17:33:55 +1000

    uploaded site to github

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by deals on 10/04/2018.
+ */
+TweenMax.to(".logo", 2, {
+    fontSize: 80,
+    left: 800,
+    paddingTop: 90,
+    paddingBottom: 20,
+    paddingLeft: 400,
+    textAlign: "center",
+    delay: 0.1,
+    stroke: "black",
+    ease: Elastic.easeInOut
+});
+TweenMax.to(".sub_logo", 2, {
+    fontSize: 32,
+    left: 600,
+    paddingLeft: 200,
+    paddingTop: 20,
+    delay: 0.1,
+    stroke: "black",
+    ease: Elastic.easeInOut
+
+});
