32aa3366182d8943215e49c02c54b7bd59c29416
Matt Deal <contact@mattdeal.com.au>
2022-03-06 22:51:23 +1100

    [Animations]: Adds some animations to the home page

--- app/components/Cards/Cards.jsx
+++ app/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import { Link } from "remix";
 import { Row } from "react-bootstrap";
 import SectionTitle from "../Sections/Section_Title/Section-title";
+import Bounce from "react-reveal/Bounce";
 
 export default function Cards() {
   return (
@@ -10,45 +11,49 @@ export default function Cards() {
         title="Projects"
         subtitle="Listed below are a few projects I have worked on"
       />
-
-      <div className="Protekt_card ">
-        <div className="card-content">
-          <p className="card-title"> Pro-Tekt Asset Protection </p>
-          <p className="card-body">
-            Pro-tekt is an Australian made GPS vehicle tracking system that
-            helps you keep track of your assets{" "}
-          </p>
-          <Link prefetch="render" to="/protekt" className="button">
-            Learn More
-          </Link>
+      <Bounce left>
+        <div className="Protekt_card ">
+          <div className="card-content">
+            <p className="card-title"> Pro-Tekt Asset Protection </p>
+            <p className="card-body">
+              Pro-tekt is an Australian made GPS vehicle tracking system that
+              helps you keep track of your assets{" "}
+            </p>
+            <Link prefetch="render" to="/protekt" className="button">
+              Learn More
+            </Link>
+          </div>
         </div>
-      </div>
-
-      <div className="La_card">
-        <div className="La_card-content">
-          <p className="La_card-title"> LA Neon Events </p>
-          <p className="card-body">
-            LA Neon Events is a Ecommerce website, that sell Neon lights, this
-            site is built fully with React and uses Storyblok as a Headless CMS.
-          </p>
-          <Link prefetch="render" to="/laneon" className="La_button">
-            Learn More
-          </Link>
+      </Bounce>
+      <Bounce clear>
+        <div className="La_card">
+          <div className="La_card-content">
+            <p className="La_card-title"> LA Neon Events </p>
+            <p className="card-body">
+              LA Neon Events is a Ecommerce website, that sell Neon lights, this
+              site is built fully with React and uses Storyblok as a Headless
+              CMS.
+            </p>
+            <Link prefetch="render" to="/laneon" className="La_button">
+              Learn More
+            </Link>
+          </div>
         </div>
-      </div>
-
-      <div className="fulcrum_card">
-        <div className="fulcrum_card-content">
-          <p className="fulcrum_card-title"> Fulcrum Software Solutions </p>
-          <p className="card-body">
-            Fulcrum Software Solutions is my business website, I help you build
-            your Dream website or business app.{" "}
-          </p>
-          <Link prefetch="render" to="/fulcrum" className="fulcrum_button">
-            Learn More
-          </Link>
+      </Bounce>
+      <Bounce right>
+        <div className="fulcrum_card">
+          <div className="fulcrum_card-content">
+            <p className="fulcrum_card-title"> Fulcrum Software Solutions </p>
+            <p className="card-body">
+              Fulcrum Software Solutions is my business website, I help you
+              build your Dream website or business app.{" "}
+            </p>
+            <Link prefetch="render" to="/fulcrum" className="fulcrum_button">
+              Learn More
+            </Link>
+          </div>
         </div>
-      </div>
+      </Bounce>
     </Row>
   );
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,4 @@
-import PhonePlaceHolder from "../../public/assets/images/devices_1.svg";
+import PhonePlaceHolder from "../../public/assets/images/devices_1.png";
 import MainButton from "~/components/button/main_button";
 import { Container, Row, Col } from "react-bootstrap";
 import Cards from "../components/Cards/Cards";
@@ -8,6 +8,7 @@ import Contact from "../components/Contact/Contact";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import Nav from "../components/Nav/nav";
 import MobileNav from "../components/Nav/mobileNav";
+import Fade from "react-reveal/Fade";
 
 export default function Home() {
   return (
@@ -20,25 +21,34 @@ export default function Home() {
           <Row>
             <Col md={2}>
               <div>
-                <div className="Responsive-Container">
-                  <h1 className="topHeader">
-                    Frontend Developer & UI / UX Designer
-                  </h1>
-                  <p className=" TopSubHeader">
-                    {" "}
-                    I build beautiful responsive websites and applications for
-                    small businesses and the every day Joe a like
-                  </p>
-                </div>
+                <Fade left>
+                  <div className="Responsive-Container">
+                    <h1 className="topHeader">
+                      Frontend Developer & UI / UX Designer
+                    </h1>
+
+                    <p className=" TopSubHeader">
+                      {" "}
+                      I build beautiful responsive websites and applications for
+                      small businesses and the every day Joe a like
+                    </p>
+                  </div>
+                </Fade>
               </div>
+
               <div>
                 <AnchorLink href="#projects">
-                  <MainButton children="Learn More" />
+                  <Fade big>
+                    <MainButton children="Learn More" />
+                  </Fade>
                 </AnchorLink>
               </div>
             </Col>
+
             <Col>
-              <img src={PhonePlaceHolder} className="PhoneImage " />
+              <Fade right>
+                <img src={PhonePlaceHolder} className="PhoneImage " />
+              </Fade>
             </Col>
           </Row>
         </Container>
@@ -47,6 +57,7 @@ export default function Home() {
         <Container id="projects">
           <Cards />
         </Container>
+
         <Container>
           <TechStack />
         </Container>
